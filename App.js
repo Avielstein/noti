209dd2d7a978715ddfd4f94f65639504c79ffe88
Aviel Stein, Ph.D <ajs568@drexel.edu>
2024-04-19 17:25:18 -0500

    added some stuff, but mostly not that useful

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Modal, StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, Modal, StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 
 
 Notifications.setNotificationHandler({
@@ -16,64 +17,95 @@ Notifications.setNotificationHandler({
 
 export default function App() {
 
+  // Function to get push token
+  const getPushToken = async () => {
+    const { data: { data } } = await Notifications.getExpoPushTokenAsync();
+    console.log("here");
+    console.log(data);
+  };
+  
+
+  //get push token
+  useEffect(() => {
+
+    // Function to check and request notification permissions
+    async function checkNotificationPermissions() {
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+
+      //if we dont have permsion, ask for it
+      if (finalStatus !== 'granted') {
+        console.log('ask user');
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      //if still not granted, tell them its required
+      if (finalStatus !== 'granted') {
+        Alert.alert(
+          'Permsion Required',
+          'Push notiications need the appropriate notifications'
+        );
+        return;
+      }
+
+
+      const pushTokenData = await Notifications.getExpoPushTokenAsync();
+      console.log(pushTokenData);
+    }
+    //call it
+    console.log('sdf');
+    checkNotificationPermissions();
+
+  }, []);
+
 
   // used to act on information when we RECEIVED the notifcaiton
-  useEffect(()=>{
-    const subscsription1 = Notifications.addNotificationReceivedListener((notification)=>{
+  useEffect(() => {
+    const subscsription1 = Notifications.addNotificationReceivedListener((notification) => {
       console.log("NOTIFICATION RECEIVED");
       console.log(notification);
     });
 
     //used to act on information when we RESPOND to notification
-    const subscsription2 = Notifications.addNotificationResponseReceivedListener((response)=>{
+    const subscsription2 = Notifications.addNotificationResponseReceivedListener((response) => {
       console.log('USER CLICKED');
       console.log(response);
     });
 
     //best practive to remove it
-    return ()=>{
+    return () => {
       subscsription1.remove();
       subscsription2.remove();
     };
-    
-  },[]);
 
-  // Function to check and request notification permissions
-  async function checkNotificationPermissions() {
-    const { status } = await Notifications.getPermissionsAsync();
-    if (status !== 'granted') {
-      const { status: newStatus } = await Notifications.requestPermissionsAsync();
-      return newStatus === 'granted';
-    }
-    return true;
-  }
+  }, []);
+
+
 
   // Function to schedule a push notification
   async function schedulePushNotification() {
-    const hasPushNotificationPermissionGranted = await checkNotificationPermissions();
 
-    if (hasPushNotificationPermissionGranted) {
-      await Notifications.scheduleNotificationAsync({
-        content: {
-          title: "You've got mail! 📬",
-          body: 'Here is the notification body',
-          data: { data: 'goes here' },
-          sound: 'default', // Use the default notification sound
-          attachments: [
-            {
-              identifier: 'imageAttachment',
-              url: 'https://picsum.photos/200/300https://fastly.picsum.photos/id/63/5000/2813.jpg?hmac=HvaeSK6WT-G9bYF_CyB2m1ARQirL8UMnygdU9W6PDvM', // URL of the image to display
-              type: 'image',
-            },
-          ],
-        },
-        trigger: { seconds: 1 }, // Change trigger time as needed
-      });
-      console.log('should be sent');
-    } else {
-      console.log('Notification permission denied');
-      // Handle notification permission denied scenario
-    }
+    getPushToken();
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "You've got mail! 📬",
+        body: 'Here is the notification body',
+        data: { data: 'goes here' },
+        sound: 'default', // Use the default notification sound
+        attachments: [
+          {
+            identifier: 'imageAttachment',
+            url: 'https://picsum.photos/200/300https://fastly.picsum.photos/id/63/5000/2813.jpg?hmac=HvaeSK6WT-G9bYF_CyB2m1ARQirL8UMnygdU9W6PDvM', // URL of the image to display
+            type: 'image',
+          },
+        ],
+      },
+      trigger: { seconds: 1 }, // Change trigger time as needed
+    });
+    console.log('should be sent');
+
   }
 
 
